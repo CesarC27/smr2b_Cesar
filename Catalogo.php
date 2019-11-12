73079b33bf04121c44c1b8eb4ff37744540ffd6f
CesarC27 <cesarcamposzarza@gmail.com>
2019-11-12 17:09:33 +0100

    catalogo.php

---
+++ Catalogo.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<style>
+div {
+  border: 1px solid black;
+  margin-top: 100px;
+  margin-bottom: 100px;
+  margin-right: 150px;
+  margin-left: 80px;
+  background-color: lightblue;
+}
+</style>
+</head>
+<body>
+
+</body>
+<html>
+	<head>
+		<style>
+			a{
+				display:block;
+				margin:10px;
+				border-style: solid;
+				border-width: thin;
+				border-radius: 5px;
+			}
+		</style>
+	<head>
+	<body>
+	    <div class="header">
+		<div><h1>LISTA DE ENLACES A PERFILES GITHUB</h1><div>
+	<a href="https://github.com/dblancoformacion" target="_blank">David Profe</a>
+	<a href="https://github.com/roodriiguezzzz" target="_blank">Rodriguez</a>
+	<a href="https://github.com/villejobsv10" target="_blank">Villegas</a>
+	<a href="https://github.com/marioperezrodriguez" target="_blank">Mario</a>
+	<a href="https://github.com/caampo03" target="_blank">Campo</a>
+	<a href="https://github.com/jesuscrespo" target="_blank">Jesus Crespo</a>
+	<a href="https://github.com/riki1" target="_blank">Riki</a>
+	</body>	
+</html>	
